7d36e076a4c422d53ca108f06832d59e04b2fb43
Yoanna <huang.yoanna@gmail.com>
2017-07-05 13:19:45 +0800

    ajax exercise

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,21 @@ class Post < ApplicationRecord
 
   validates_presence_of :content
   belongs_to :user
-  
+  belongs_to :category, :optional => true
+  has_many :scores, :class_name => "PostScore"
+  has_many :likes, :dependent => :destroy
+  has_many :liked_users, :through => :likes, :source => :user
+
+  def find_like(user)
+    self.likes.where( :user_id => user.id ).first
+  end
+
+  def find_score(user)
+    user && self.scores.where( :user_id => user.id ).first
+  end
+
+  def average_score
+    self.scores.average(:score)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
+  has_many :likes, :dependent => :destroy
+  has_many :liked_posts, :through => :likes, :source => :post
 
   def display_name
     # #取 email 的前半來顯示，如果你也可以另開一個字段 nickname 讓用戶可以自己編輯顯示名稱
     self.email.split("@").first
   end
-  
+
+  def is_admin?
+    role == "admin"
+  end
+
 end
